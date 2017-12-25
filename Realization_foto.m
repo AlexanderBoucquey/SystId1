662ed1f70565c7b73940896509c9ecfccd27d0a9
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-25 16:36:10 +0100

    added some pictures

--- Realization_foto.m
+++ Realization_foto.m
@@ -1,21 +1,25 @@
-clear all
-close all
+clear variables;
+close all;
 
+% Read the data.
 data = imread('foto.JPG');
 A = im2double(data);
 A = rgb2gray(A);
-%figure();
-%imshow(A);
-hold off
+
+% SVD of A.
 [U, S, V] = svd(A);
+
+% Plot singular values.
 s = diag(S);
 [m , n] = size(S);
 grid = linspace(1, m, m);
-%figure();
-%loglog(grid, s);
-hold off
-% A1
-for i=[1,2,5,10,20,50,100]
+figure();
+plot(grid,s, 'ro');
+xlabel('Index single value');
+ylabel('Value');
+
+% Show figures calculated with reduced information.
+for i=[1,2,5,10,20,100]
     figure();
     dr = (m * i+ i + n * i)/(m*n)*100;    
     A1 = U(:,1:i)*S(1:i,1:i)*V(:,1:i)';
