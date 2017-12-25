d72b708b3a9baf854642a1b7134b6157668ade90
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-25 18:53:50 +0100

    frequentie gebied

--- Notch_Filter.m
+++ Notch_Filter.m
@@ -13,22 +13,20 @@ f_sin = repmat(sin(f*2*pi),[1 20]);
 
 x = linspace(0, 2*pi, L);
 figure()
-%plot(x, f_cos(1:N))
-%hold on
-%plot(x, f_sin(1:N))
-%hold off
+
 
 % Opgave 2.2
 Fs = 1;
 b = y;
 A = [f_cos' f_sin'];
 xh = A\b;
-plot(x, A*xh-b)
 Y = fft(A*xh-b);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
+
+% Plot figure
 semilogy(f,P1) 
 xlim([0 0.1])
 title('Single-Sided Amplitude Spectrum of X(t)')
@@ -38,12 +36,14 @@ ylabel('|P1(f)|')
 % In frequentiedomein
 b_f = fft(y);
 H = ones(L,1);
-H(N) = 0;
+H(N/2) = 10^(-16); % OPLETTEN, (klopt dit wel?)
 xh_f = b_f.* H;
-figure()
 P2 = abs(xh_f/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(L-1)/2)/L;
-semilogy(f, P2);
+f = Fs*(0:(L/2))/L;
 
+% Plot figure
+figure()
+semilogy(f, P1);
+xlim([0 0.1])
