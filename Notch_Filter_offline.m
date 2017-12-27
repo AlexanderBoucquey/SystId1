6b68322c451485fe25853bacfac3aa7709dc64c9
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-27 22:26:32 +0100

    notch filter af

--- Notch_Filter_offline.m
+++ Notch_Filter_offline.m
@@ -10,7 +10,13 @@ fs = 1/N;
 f = 0:fs:fs*(N-1);
 f_cos = repmat(cos(f*2*pi),[1 20]);
 f_sin = repmat(sin(f*2*pi),[1 20]);
+k = 1;
 
+N2 = 25;
+fs2 = 1/N2;
+f2 = 0:fs2:fs2*(N-1);
+f_cos2 = repmat(cos(f2*2*pi),[1 20]);
+f_sin2 = repmat(sin(f2*2*pi),[1 20]);
 x = linspace(0, 2*pi, L);
 figure()
 
@@ -29,14 +35,16 @@ f = Fs*(0:(L/2))/L;
 % Plot figure
 semilogy(f,P1) 
 xlim([0 0.1])
+
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
 % In frequentiedomein
 b_f = fft(y);
+F = 1/N;
 H = ones(L,1);
-H(N/2) = 10^(-16); % OPLETTEN, (klopt dit wel?)
+H(F*L+1) = 0;
 xh_f = b_f.* H;
 P2 = abs(xh_f/L);
 P1 = P2(1:L/2+1);
@@ -47,3 +55,59 @@ f = Fs*(0:(L/2))/L;
 figure()
 semilogy(f, P1);
 xlim([0 0.1])
+
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+% Opgave 2.3
+A_data = [f_cos' f_sin'];
+a = A_data(1,:);
+x = A_data(1,:)\y(1);
+P = eye(2);
+A = P*a'/(1+a*P*a');
+for i = 1:k
+    a = A_data(i,:);
+    x = x - A*(a*x-y(i));
+    A = P*a'/(1+a*P*a');
+    P = P - P*a'*a*P/(1+a*P*a');
+    X(i) = norm(xh-x);
+end
+
+Y = fft(A_data*x-y);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = (0:(L/2))/L;
+figure()
+semilogy(f, P1);
+xlim([0 0.1])
+
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+hold on
+Y = fft(y);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = (0:(L/2))/L;
+semilogy(f,P1,'r');
+legend('Filtered Signal','Original Signal');
+
+figure();
+semilogy(X,'b');
+xlabel('$k$');
+ylabel('||x_(exact)-x_(estimated)');
+
+% Plot the original signal, the LS frequency filtered signal, the RLS
+% filtered signal.
+figure()
+plot(y,'r');
+hold on
+plot(y-A_data*xh, 'b');
+hold on
+plot(y-A_data*x, 'g');
+xlim([0 10]);
+legend('Original Signal','Offline Filtered Signal', 'Online Filtered Signal', 'Location','northwest');
--- Notch_Filter_online.m
+++ Notch_Filter_online.m
@@ -10,6 +10,48 @@ fs = 1/N;
 f = 0:fs:fs*(N-1);
 f_cos = repmat(cos(f*2*pi),[1 20]);
 f_sin = repmat(sin(f*2*pi),[1 20]);
-x = linspace(0, 2*pi, L);
+k = 880
 
 % Opgave 2.3
+A_data = [f_cos' f_sin'];
+a = A_data(1,:);
+x = A_data(1,:)\y(1);
+P = eye(2);
+A = P*a'/(1+a*P*a');
+for i = 1:k
+    a = A_data(i,:);
+    x = x - A*(a*x-y(i));
+    A = P*a'/(1+a*P*a');
+    P = P - P*a'*a*P/(1+a*P*a');
+    X(i,:) = x;
+end
+
+Y = fft(A_data*x-y);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = (0:(L/2))/L;
+figure()
+semilogy(f, P1);
+xlim([0 0.1])
+
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+hold on
+Y = fft(y);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = (0:(L/2))/L;
+semilogy(f,P1,'r');
+legend('Filtered Signal','Original Signal');
+
+figure();
+plot(X(:,1),'b');
+hold on
+plot(X(:,2),'r');
+xlabel('$k$');
+ylabel('$x_i$');
+
