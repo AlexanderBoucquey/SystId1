186a8e58c2480429f65c743348207795b2148074
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-29 17:51:35 +0100

    eerste aanpassingen

--- real_id_problem/real_problem.m
+++ real_id_problem/real_problem.m
@@ -1,5 +1,7 @@
-clear all
+clear variables
 close all
+
+% Initialisatie van het probleem. (bepaal input u).
 u = randn(1000,1);
 index = u<=3;
 u = u(index);
@@ -7,10 +9,14 @@ index = u>=-3;
 u = u(index);
 %u=zeros(1000,1);
 u(1) = 1;
+
+% Experiment + output (y);
 y = exercise2(u);
 poly = spline(u,y);
 
-
+% Preprocessing stap 1: Verwijderen van delay.
 cra([y,u]);
 
+% Preprocessing stap 2: Verwijderen pieken).
+y = pkshave(y, [-2,2], 1);
 save real_problem;
