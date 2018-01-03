66b831e7df1611cd0dc83d1de002395204f561df
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2018-01-03 22:40:40 +0100

    beetje geklungel

--- real_id_problem/real_problem.m
+++ real_id_problem/real_problem.m
@@ -4,25 +4,32 @@ close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialisatie van het probleem. (bepaal input u).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+F = 20;         % Frequentie
+Fs = 1000;      % Sample frequentie
+T = 1/Fs;       % Sample periode
+L = 1000;       % Lengte van signaal
+t = (0:L-1)*T;  % Tijdsvector
+
 
 % White noise
+% sigma_u = 0.5;
 % u = randn(1000,1);
 
 % Zero input signal
 % u = zeros(1000,1);
 
 % Colored noise
-% u = randn(1000,1);
-% [b_butter, a_butter] = butter(4,0.2,'high');
-% u = filter(b_butter,a_butter,u);
+sigma_u = 0.5;
+u = randn(1000,1);
+u = sigma_u*u;
+[b_butter, a_butter] = butter(4,0.2,'high');
+u = filter(b_butter,a_butter,u);
 
 % Sinus wave
-F = 20; % Frequentie
-% grid = 0:F*2*pi/1000:F*2*pi;
-% u = sin(grid)';
+% u = sin(2*pi*F*t');
 
 % Step function
-u = ones(1000,1);
+% u = ones(1000,1);
 % u(1) = 0;
 
 % Impulse function
@@ -39,6 +46,7 @@ u = u(index);
 % Plot input signal
 figure();
 plot(u,'r');
+title('Input signaal');
 hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -47,7 +55,9 @@ hold off
 y = exercise2(u);
 %poly = spline(u,y);
 
-
+figure()
+plot(y,'r');
+title('Output signaal');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Preprocessing
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -55,26 +65,31 @@ y = exercise2(u);
 % ir = cra([y,u],20,10,1);
 % [~,R,~] = cra([y,u]);
 % Preprocessing stap 2: Verwijderen pieken).
-y = pkshave(y, [25,35], 1);
+y1 = pkshave(y, [25,35], 1);
 
 % Preprocessing stap 3: Verwijderen van trends.
-y = detrend(y);
+y = detrend(y1);
 
 
-hold off;
+figure()
+plot(y1,'b');
+hold on
 plot(y,'r');
+legend('pkshave','detrend');
+title('Output signaal, gedetrend en pkshaved');
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Identificeren van model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Schat aantal parameters mbv aic
-N = 800;
-I = 500;
+N = L;
+I = 900;
 grid = linspace(1,I,I);
 
 % Bereken de Toeplitz matrix
 for j = 1:I
-    K = toeplitz(u(:,1), [u(1,1) zeros(1,I)] );
+    K = toeplitz(u(:,1), [u(1,1) zeros(1,I-1)] );
 end
 g = K\y;
 g = g';
@@ -85,35 +100,46 @@ V_aic = zeros(I,1);
 for t = 1:I
     yh = filter(g(1,1:t),1,u);
     V_ls(t,1) = 0;
-    for i = 1:N
+    for i = 1:size(yh(:,1))
         V_ls(t,1) = V_ls(t,1) + (y(i,1)-yh(i,1))^2;
     end
-    V_aic(t,1) = V_ls(t,1)*(1+2*t/N)/N;
+    V_aic(t,1) = V_ls(t,1)*(1+2*t/N)/(N*sigma_u^2);
+    V_ls(t,1) = V_ls(t,1)/(N*sigma_u^2);
 end
 
 figure()
-plot(grid,V_aic(:,1),'-');
+semilogy(grid,V_ls(:,1),'--');
+hold on
+semilogy(grid,V_aic(:,1),'-');
+xlabel('Order');
+ylabel('Cost');
+xlim([0 I]);
+legend('est', 'AIC');
+title('Aantal param schatten');
 %impulsresponsfunctie
-figure()
-IR = cra([y u]);
+% figure()
+% IR = cra([y u]);
 
 
-%fft dit klopt nog niet
-L = 1000;
+% FFT 
 Y = fft(y);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-f = F*(0:(L/2))/L*5;
+f = Fs*(0:(L/2))/L;
 
 % Plot figure
 figure
-semilogy(f,P1)
-
+plot(f,P1)
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+xlim([0 100]);
+hold off
 
 %verschillende modellen
-sysarx = arx([y u],[ 100 100 0]);
-sysarmax = armax([y u],[10 10 10 0]);
-sysoe = oe([y u], [10 10 0]);
-sysbj = bj([y u],[10 10 10 10 0]);
-save real_problem;
+% sysarx = arx([y u],[ 100 100 0]);
+% sysarmax = armax([y u],[10 10 10 0]);
+% sysoe = oe([y u], [10 10 0]);
+% sysbj = bj([y u],[10 10 10 10 0]);
+% save real_problem;
